61ca199c55eaa59b715bb9f127cdcc955efaef23
Fozan3060 <fozanjavaid3060@outlook.com>
2024-04-09 15:31:43 +0500

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="footer__wrapper bg-footer shadow-footer rounded-footer flex-col lg:flex-row  flex items-center justify-between p-4">
           <div className="logo mb-4 lg:m-0 md:text-2xl font-bold cursor-pointer">SMO</div>
           <div className="copy-right font-bold text-center text-sm md:text-base">
-            &copy; 2024 Murad Yusubov. All rights reserved. <br/> Image from <a href="https://www.freepik.com" target="_blank">Freepik</a>
+            &copy; 2024 Fozan Javajd. All rights reserved. <br/> Image from <a href="https://www.freepik.com" target="_blank">Freepik</a>
           </div>
         </div>
       </div>
